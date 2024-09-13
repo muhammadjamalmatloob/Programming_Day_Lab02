1371b2d11b5ba18827845c392b8fd742d2f9575d
muhammadjamalmatloob <jamalmatloob786@gmail.com>
2024-09-12 22:26:31 -0700

    commit

--- bowl.cpp
+++ bowl.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-main(){
+main()
+{
   cout << "     *   *   *             " << endl;
   cout << "       *   *   *           " << endl;
   cout << "        *   *   *          " << endl;
